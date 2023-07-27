6ad80214054dfd114adf294395a90040014504b4
musangi <faithmusangi50@gmail.com>
2023-07-27 20:45:48 +0300

    function to get computer choice

---
+++ app.js
@@ -0,0 +1,11 @@
+const options = ["rock", "paper", "scissors"];
+
+
+function getComputerChoice() {
+    const choice = options[Math.floor(Math.random()*options.length)];
+    return choice
+
+}
+// console.log(computerSelection);
+
+
