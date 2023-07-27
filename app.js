d1013a4bbb6f545fb05808bc26a996e018f6d627
musangi <faithmusangi50@gmail.com>
2023-07-27 20:47:00 +0300

    function to play a round of Rochambeau and display who wins in each round

--- app.js
+++ app.js
@@ -8,4 +8,24 @@ function getComputerChoice() {
 }
 // console.log(computerSelection);
 
+function playRound(playerSelection, computerSelection ) {
+    computerSelection = getComputerChoice().toLocaleLowerCase();
+    playerSelection = playerSelection.toLocaleLowerCase();
+
+    if(computerSelection == playerSelection){
+        return "You tied";
+    } else if (computerSelection == "rock" && playerSelection == "scissors" ||
+    computerSelection == "paper" && playerSelection == "rock" ||
+    computerSelection == "scissors" && playerSelection == "paper") {
+        return `You lose ${computerSelection} beats ${playerSelection}`;
+        
+    }else {
+        return `You win ${playerSelection} beats ${computerSelection}`;
+    }
+    
+}
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
 
