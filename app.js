78eca0da2fcee8e97a6150eb214da7354f9fd62c
Faith <faithmusangi50@gmail.com>
2023-08-17 12:08:07 +0300

    Display the running score, and announce a winner of the game once one player reaches 5 points.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const options = ["rock", "paper", "scissors"];
+let computerScore = 0;
+let playerScore = 0;
+let roundCount = 0;
 
 
 function getComputerChoice() {
@@ -6,24 +9,40 @@ function getComputerChoice() {
     return choice
 
 }
-// console.log(computerSelection);
 
 function playRound(playerSelection, computerSelection ) {
     computerSelection = getComputerChoice().toLocaleLowerCase();
     playerSelection = playerSelection.toLocaleLowerCase();
 
-    if(computerSelection == playerSelection){
+    if(computerSelection === playerSelection){
         return "You tied";
-    } else if (computerSelection == "rock" && playerSelection == "scissors" ||
-    computerSelection == "paper" && playerSelection == "rock" ||
-    computerSelection == "scissors" && playerSelection == "paper") {
+    } else if (computerSelection === "rock" && playerSelection === "scissors" ||
+    computerSelection === "paper" && playerSelection === "rock" ||
+    computerSelection === "scissors" && playerSelection === "paper") {
+        computerScore++;
         return `You lose ${computerSelection} beats ${playerSelection}`;
         
     }else {
+        playerScore++;
         return `You win ${playerSelection} beats ${computerSelection}`;
     }
 
     
+}
+
+function declareWinner() {
+    let winner = "it's a tie";
+    if(playerScore > computerScore){
+        winner = "You win!";
+    } else if (computerScore > playerScore){
+        winner = "Computer wins!";
+    }
+    results.textContent = `${winner} Final Score: Player ${playerScore} - Computer ${computerScore}`;
+
+    // reset the score for the next game
+    playerScore = 0;
+    computerScore = 0;
+    roundCount = 0;
 }
 
 const buttons = document.querySelectorAll('button');
@@ -32,17 +51,18 @@ const results   = document.getElementById('results');
 buttons.forEach(button => {
     button.addEventListener('click', function() {
         const playerSelection = button.id;
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection, computerSelection);
-        console.log(result);
+        const result = playRound(playerSelection);
         results.textContent = result;
+
+        roundCount++;
+
+        if(roundCount === 5){
+            declareWinner();
+        }
+
     });
 });
 
-// const playerSelection = prompt();
-// const computerSelection = getComputerChoice();
-
-// console.log(playRound(playerSelection, computerSelection));
 
 
 
